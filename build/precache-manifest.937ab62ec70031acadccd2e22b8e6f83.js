be2eb15a11e49ff9e4b677cc27cfc57850ec0a74
Akos11 <36598710+Akos11@users.noreply.github.com>
2020-04-24 22:00:18 +0200

    commit build

---
+++ build/precache-manifest.937ab62ec70031acadccd2e22b8e6f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfd33b0e06d20386a78e39e209f3146",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afaeaa7a8f21619df6cf",
+    "url": "./static/css/main.9216663f.chunk.css"
+  },
+  {
+    "revision": "9c43dcac9fb25086c010",
+    "url": "./static/js/2.f9671187.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f9671187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afaeaa7a8f21619df6cf",
+    "url": "./static/js/main.7a8c4480.chunk.js"
+  },
+  {
+    "revision": "13581df0e096de695991",
+    "url": "./static/js/runtime-main.3fe63c3a.js"
+  }
+]);
