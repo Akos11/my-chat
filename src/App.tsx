d8b4279b3798b35d66734739cc7a03964f4f4983
Akos11 <36598710+Akos11@users.noreply.github.com>
2020-04-24 21:58:45 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { proxy } from './Proxy';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Login } from './Login'
+import { Main } from './Main'
+
+
+export default class App extends Component {
+  state = { loggedIn : false };
+  render() {
+    proxy.addEventListener("login", () => this.setState({loggedIn : true}), this);
+    return (
+      <div className="app">
+        {!this.state.loggedIn &&< Login />}
+        {this.state.loggedIn &&< Main />}
+      </div>
+    );
+  }
 }
 
-export default App;
